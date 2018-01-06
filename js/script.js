9e2511906b0c3c4f2d06bac1955cefac5cbdc1f8
andrearizzo-MT <andrearizzo85@gmail.com>
2018-01-07 00:54:07 +0100

    Photon API requests refinements and not connected catered for

--- js/script.js
+++ js/script.js
@@ -4,35 +4,48 @@ function getThePunisher(){
   $('.punisher').css('display', 'none');
 
   //PARTICLE PHOTON API SCHEMA
+  var deviceStats = "https://api.particle.io/v1/devices/34003f001447353136383631?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
   var batteryVoltage = "https://api.particle.io/v1/devices/34003f001447353136383631/voltage?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
   var batteryPercentage = "https://api.particle.io/v1/devices/34003f001447353136383631/percentage?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
-  //GET VOLTAGE REQUEST
+
+  //GET STATS REQUEST
   $.ajax({
     type: 'GET',
-    url: batteryVoltage,
-    success: function(voltage){
-      console.log('success',voltage);
+    url: deviceStats,
+    success: function(stats){
+      console.log('success',stats);
+      //ASSIGNING JSON TO HTML
+      photonName.innerHTML = stats.name;
+      photonConnected.innerHTML = stats.connected;
+      photonVoltage.innerHTML = "N/A"
+      photonPercentage.innerHTML = "N/A"
 
-      //ASSIGNING PHOTON PARAMETERS TO HTML
-      photonVoltage.innerHTML = voltage.result.toFixed(2) + " V";
+      if (stats.connected = false){
+        exit();
+      } else {
+        //GET VOLTAGE REQUEST
+        $.ajax({
+          type: 'GET',
+          url: batteryVoltage,
+          success: function(voltage){
+            console.log('success',voltage);
+            //GET PERCENTAGE REQUEST
+            $.ajax({
+              type: 'GET',
+              url: batteryPercentage,
+              success: function(percentage){
+                console.log('success',percentage);
 
-      $.ajax({
-        type: 'GET',
-        url: batteryPercentage,
-        success: function(percentage){
-          console.log('success',percentage);
+                //ASSIGNING JSON TO HTML
+                photonVoltage.innerHTML = voltage.result.toFixed(2) + " V";
+                photonPercentage.innerHTML = percentage.result.toFixed(0) + " %";
 
-          //ASSIGNING PHOTON PARAMETERS TO HTML
-          photonPercentage.innerHTML = percentage.result.toFixed(0) + " %";
-          photonConnected.innerHTML = percentage.coreInfo.connected;
-
-          $('.punisher').css('display', 'block');
-        }
-      })
-
+              }
+            })
+          }
+        })
+      }
+    $('.punisher').css('display', 'block');
     }
   })
-
-
-
 }
