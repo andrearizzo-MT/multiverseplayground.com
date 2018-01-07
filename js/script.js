3fb0d8a08c3c87163a8aff06f63e75cc763e6554
andrearizzo-MT <andrearizzo85@gmail.com>
2018-01-07 15:57:52 +0100

    Photon API + UI refinements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+//HIDE FOOTER
+function hideFooter() {
+    var x = document.getElementById("footerStats");
+        x.style.display = "none";
+}
+
+function showFooter() {
+    var x = document.getElementById("footerStats");
+        x.style.display = "block";
+}
+
 //PARTICLE PHOTON GET REQUESTS
 function getThePunisher(){
-
-  $('.punisher').css('display', 'none');
 
   //PARTICLE PHOTON API SCHEMA
   var deviceStats = "https://api.particle.io/v1/devices/34003f001447353136383631?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
   var batteryVoltage = "https://api.particle.io/v1/devices/34003f001447353136383631/voltage?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
   var batteryPercentage = "https://api.particle.io/v1/devices/34003f001447353136383631/percentage?access_token=45b47fb7b84f6200ba7a9decf2788b1098394215"
+  var connected = ""
 
   //GET STATS REQUEST
   $.ajax({
@@ -14,13 +24,19 @@ function getThePunisher(){
     url: deviceStats,
     success: function(stats){
       console.log('success',stats);
-      //ASSIGNING JSON TO HTML
-      photonName.innerHTML = stats.name;
-      photonConnected.innerHTML = stats.connected;
-      photonVoltage.innerHTML = "N/A"
-      photonPercentage.innerHTML = "N/A"
 
-      if (stats.connected = false){
+      //ASSIGNING VARIABLES AND INNERHTML VALUES
+      connected = stats.connected;
+      // photonName.innerHTML = stats.name;
+      // photonConnected.innerHTML = "-------------";
+      // photonVoltage.innerHTML = "--" + " V";
+      // photonPercentage.innerHTML = "--" + " %"
+
+      if (connected = false){
+        photonName.innerHTML = stats.name;
+        photonConnected.innerHTML = "OFFLINE " + '<i class="fas fa-times-circle"></i>';
+        photonVoltage.innerHTML = "--" + "V";
+        photonPercentage.innerHTML = "--" + "%"
         exit();
       } else {
         //GET VOLTAGE REQUEST
@@ -36,16 +52,20 @@ function getThePunisher(){
               success: function(percentage){
                 console.log('success',percentage);
 
-                //ASSIGNING JSON TO HTML
-                photonVoltage.innerHTML = voltage.result.toFixed(2) + " V";
-                photonPercentage.innerHTML = percentage.result.toFixed(0) + " %";
-
+                //ASSIGNING JSON TO INNERHTML
+                if(connected = true){
+                  photonConnected.innerHTML = "ONLINE " + '<i class="fas fa-check-circle"></i>';
+                }
+                photonName.innerHTML = stats.name;
+                photonVoltage.innerHTML = voltage.result.toFixed(1) + "V";
+                photonPercentage.innerHTML = percentage.result.toFixed(1) + "%";
+                showFooter();
               }
             })
           }
         })
       }
-    $('.punisher').css('display', 'block');
+      setTimeout(function(){getThePunisher();}, 10000);
     }
   })
 }
