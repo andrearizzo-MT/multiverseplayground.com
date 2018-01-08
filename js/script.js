3df0d5b2e745e3be81fa69c7b0099d76e2818d7b
andrearizzo-MT <andrearizzo85@gmail.com>
2018-01-08 23:56:44 +0100

    Further UI refining, icons instead of text and better mob responsiveness

--- js/script.js
+++ js/script.js
@@ -24,7 +24,8 @@ function getThePunisher(){
     url: deviceStats,
     success: function(stats){
       console.log('success',stats);
-
+      //JSON Time GMT i.e. + 1 since GMT+1 for MALTA
+      var hourPlusOne = parseInt(stats.last_heard.slice(11,13)) + 1;
       //ASSIGNING VARIABLES AND INNERHTML VALUES
       connected = stats.connected;
       // photonName.innerHTML = stats.name;
@@ -33,11 +34,12 @@ function getThePunisher(){
       // photonPercentage.innerHTML = "--" + " %"
 
       if (connected === false){
-        photonName.innerHTML = stats.name;
-        photonConnected.innerHTML = '<i class="fas fa-times-circle w3-text-red"></i>' + " OFFLINE";
-        photonLastPing.innerHTML = stats.last_heard.slice(0,10) + " " + '<i class="fas fa-space-shuttle w3-text-light-green"></i>' + " " + stats.last_heard.slice(11,19);
-        photonVoltage.innerHTML = "n/a";
-        photonPercentage.innerHTML = "n/a";
+        photonPercentage.innerHTML = '<i class="fas fa-battery-empty w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + "n/a";
+        photonName.innerHTML = '<i class="fas fa-fighter-jet w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + stats.name;
+        photonConnected.innerHTML = '<i class="fas fa-plug w3-text-dark-grey fa-lg fa-rotate-90"></i>&nbsp;&nbsp;&nbsp;' + '<i class="fas fa-times-circle w3-text-red"></i>'; //OFFLINE
+        photonLastTime.innerHTML = '<i class="far fa-clock w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + hourPlusOne + stats.last_heard.slice(13,19);
+        photonLastDate.innerHTML = '<i class="fas fa-calendar-alt w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + stats.last_heard.slice(0,10);
+        photonVoltage.innerHTML = '<i class="fab fa-medapps w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;&nbsp;' + "n/a";
         showFooter();
       } else if (connected === true) {
         //GET VOLTAGE REQUEST
@@ -52,21 +54,35 @@ function getThePunisher(){
               url: batteryPercentage,
               success: function(percentage){
                 console.log('success',percentage);
+                //JSON Time GMT i.e. + 1 since GMT+1 for MALTA
+                var hourPlusOne = parseInt(stats.last_heard.slice(11,13)) + 1;
+
                 //ASSIGNING JSON TO INNERHTML
-                if(connected === true){
-                  photonConnected.innerHTML = '<i class="fas fa-check-circle w3-text-green"></i>' + " ONLINE";
+                if (percentage.result.toFixed(0) < 15){
+                  photonPercentage.innerHTML = '<i class="fas fa-battery-empty w3-text-red fa-lg"></i>&nbsp;&nbsp;' + percentage.result.toFixed(0) + '<i class="fas fa-percent"></i>';
+                } else if (percentage.result.toFixed(0) >= 15 && percentage.result.toFixed(0) <= 25){
+                  photonPercentage.innerHTML = '<i class="fas fa-battery-quarter w3-text-orange fa-lg"></i>&nbsp;&nbsp;' + percentage.result.toFixed(0) + "%";
+                } else if (percentage.result.toFixed(0) == 50 || percentage.result.toFixed(0) < 50 && percentage.result.toFixed(0) > 25 ){
+                  photonPercentage.innerHTML = '<i class="fas fa-battery-half w3-text-yellow fa-lg"></i>&nbsp;&nbsp;' + percentage.result.toFixed(0) + "%";
+                } else if (percentage.result.toFixed(0) == 75 || percentage.result.toFixed(0) < 75 && percentage.result.toFixed(0) > 50 ){
+                  photonPercentage.innerHTML = '<i class="fas fa-battery-three-quarters w3-text-green fa-lg"></i>&nbsp;&nbsp;' + percentage.result.toFixed(0) + "%";
+                } else if (percentage.result.toFixed(0) == 100 || percentage.result.toFixed(0) < 100 && percentage.result.toFixed(0) > 75 ){
+                  photonPercentage.innerHTML = '<i class="fas fa-battery-full w3-text-white fa-lg"></i>&nbsp;&nbsp;' + percentage.result.toFixed(0) + '<i class="fas fa-percent fa-xs"></i>';
                 }
-                photonName.innerHTML = stats.name;
-                photonLastPing.innerHTML = stats.last_heard.slice(0,10) + " " + '<i class="fas fa-space-shuttle w3-text-light-green"></i>' + " " + stats.last_heard.slice(11,19);
-                photonVoltage.innerHTML = voltage.result.toFixed(1) + "V";
-                photonPercentage.innerHTML = percentage.result.toFixed(1) + "%";
+                photonName.innerHTML = '<i class="fas fa-fighter-jet w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + stats.name;
+                if(connected === true){
+                  photonConnected.innerHTML = '<i class="fas fa-plug w3-text-dark-grey fa-lg fa-rotate-90"></i>&nbsp;&nbsp;&nbsp;' + '<i class="fas fa-check-circle w3-text-green fa-lg"></i>'; //ONLINE
+                }
+                photonLastTime.innerHTML = '<i class="far fa-clock w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + hourPlusOne + stats.last_heard.slice(13,19);
+                photonLastDate.innerHTML = '<i class="fas fa-calendar-alt w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;' + stats.last_heard.slice(0,10);
+                photonVoltage.innerHTML = '<i class="fab fa-medapps w3-text-dark-grey fa-lg"></i>&nbsp;&nbsp;&nbsp;' + voltage.result.toFixed(2) + "V";
                 showFooter();
               }
             })
           }
         })
       }
-      setTimeout(function(){getThePunisher();}, 10000);
+      setTimeout(function(){getThePunisher();}, 60000);
     }
   })
 }
